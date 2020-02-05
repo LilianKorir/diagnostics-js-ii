ce54627b668b126aa79f424e7251095ddcbeae24
Lilian Korir <likorir@davidson.edu>
2020-02-05 15:48:33 -0800

    timesTable trial

--- exercises/timesTable13.js
+++ exercises/timesTable13.js
@@ -8,11 +8,24 @@
  *   13 times 3 is 39
  *   13 times 4 is 52
  */
-function timesTable13(n) {
+console.log('eeee');
+ function timesTable13(n) {
+
+
+for (num = 1; num <= n; num += 1){
+
+//console.log( 13 * num)
+console.log('13 times' + ' ', + num ,'is', + num * 13)
+
+  }
+
+ }
+
+
   // This is your job. :)
 
   // Use console.log to print out statements like "13 times 5 is 65"
-}
+//}
 
 if (require.main === module) {
   let input;
@@ -20,13 +33,18 @@ if (require.main === module) {
   input = 3;
   console.log('The first', input, 'multiples of 13 are:')
   timesTable13(input);
-  console.log('----------------');
+  //console.log('13 times' + ' ', + num ,'is', + 13 * num );
+
   console.log();
 
 
   input = 5;
   console.log('The first', input, 'multiples of 13 are:')
   timesTable13(input);
-  console.log('----------------');
+  //console.log('13 times', input, 'is', timesTable13(input));
   console.log();
+
+  input = 8;
+  console.log('The first', input, 'multiples of 13 are:');
+  timesTable13(input);
 }
